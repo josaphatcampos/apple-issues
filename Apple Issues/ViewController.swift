830d2279460be450881296a25450e006f7ca485a
Josaphat Campos <josaphat.campos@yahoo.com.br>
2020-04-20 22:11:20 -0300

    Initial Commit

---
+++ Apple Issues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Issues
+//
+//  Created by Josaphat Campos Pereira on 20/04/20.
+//  Copyright © 2020 Josaphat Campos Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
